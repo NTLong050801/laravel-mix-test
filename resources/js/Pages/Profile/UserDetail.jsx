d13664ec86e7925724c12555b8b1d0d2ef069b79
NTLong050801 <90180848+NTLong050801@users.noreply.github.com>
2023-03-20 19:57:43 +0700

    update

---
+++ resources/js/Pages/Profile/UserDetail.jsx
@@ -0,0 +1,19 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import DeleteUserForm from './Partials/DeleteUserForm';
+import UpdatePasswordForm from './Partials/UpdatePasswordForm';
+import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
+import {Head} from '@inertiajs/react';
+
+import axios from 'axios';
+
+export default function UserDetail({auth, mustVerifyEmail, status}) {
+
+    return (
+        <div>
+            12312
+        </div>
+    )
+}
+
+
+
